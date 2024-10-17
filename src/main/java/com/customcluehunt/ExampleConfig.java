7a0991c31d396ca227f69cacf3a60be13e6d6746
pogofcz <pogof@pogof.cz>
2024-10-17 02:13:43 +0200

    I am committing yey

--- src/main/java/com/customcluehunt/ExampleConfig.java
+++ src/main/java/com/customcluehunt/ExampleConfig.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.customcluehunt;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
@@ -14,6 +14,6 @@ public interface ExampleConfig extends Config
 	)
 	default String greeting()
 	{
-		return "Hello";
+		return "Will this work?";
 	}
 }
